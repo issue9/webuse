beec42f8c4736cccd01238a7e235ccc9ca97c06a
caixw <xnotepad@gmail.com>
2015-11-29 22:07:05 +0800

    将mux下部分handler接口实现独立成包

---
+++ recovery_test.go
@@ -0,0 +1,46 @@
+// Copyright 2015 by caixw, All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/issue9/assert"
+)
+
+var _ RecoverFunc = defaultRecoverFunc
+var _ RecoverFunc = PrintDebug
+
+func TestDefaultRecoverFunc(t *testing.T) {
+	a := assert.New(t)
+	w := httptest.NewRecorder()
+	a.NotNil(w)
+
+	defaultRecoverFunc(w, "not found")
+	a.Equal(http.StatusText(404)+"\n", w.Body.String())
+}
+
+func TestNewRecovery(t *testing.T) {
+	a := assert.New(t)
+
+	// h参数传递空值
+	a.Panic(func() {
+		NewRecovery(nil, nil)
+	})
+
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	// 指定fun参数为nil，能正确设置其值
+	r := NewRecovery(h, nil)
+	a.NotNil(r.recoverFunc)
+}
+
+func TestPrintDebug(t *testing.T) {
+	w := httptest.NewRecorder()
+	PrintDebug(w, "PrintDebug")
+	t.Log(w.Body.String())
+}
