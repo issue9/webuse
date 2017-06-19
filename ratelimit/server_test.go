5e65bf1e6eae2595e841d8013488cb2ef3e76778
caixw <xnotepad@gmail.com>
2017-06-20 00:17:39 +0800

    添加测试内容

--- ratelimit/server_test.go
+++ ratelimit/server_test.go
@@ -4,4 +4,61 @@
 
 package ratelimit
 
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/issue9/assert"
+)
+
 var _ GenFunc = genIP
+
+func TestServer_bucket(t *testing.T) {
+	a := assert.New(t)
+	srv := NewServer(NewMemory(10), 10, 50*time.Second, nil)
+	a.NotNil(srv)
+
+	r1 := httptest.NewRequest(http.MethodGet, "/test", nil)
+	r1.RemoteAddr = "1"
+	r2 := httptest.NewRequest(http.MethodGet, "/test", nil)
+	r2.RemoteAddr = "1"
+
+	b1, err := srv.bucket(r1)
+	a.NotError(err).NotNil(b1)
+	b2, err := srv.bucket(r2)
+	a.NotError(err).NotNil(b2)
+	a.Equal(b1, b2)
+
+	r2.RemoteAddr = "2"
+	b2, err = srv.bucket(r2)
+	a.NotError(err).NotNil(b2)
+	a.NotEqual(b1, b2)
+
+	r2.RemoteAddr = ""
+	b2, err = srv.bucket(r2)
+	a.Error(err).Nil(b2)
+}
+
+func TestServer_transfer(t *testing.T) {
+	a := assert.New(t)
+	srv := NewServer(NewMemory(10), 10, 50*time.Second, nil)
+	a.NotNil(srv)
+
+	r1 := httptest.NewRequest(http.MethodGet, "/test", nil)
+	r1.RemoteAddr = "1"
+	r2 := httptest.NewRequest(http.MethodGet, "/test", nil)
+	r2.RemoteAddr = "2"
+
+	b1, err := srv.bucket(r1)
+	a.NotError(err).NotNil(b1)
+	b2, err := srv.bucket(r2)
+	a.NotError(err).NotNil(b2)
+	a.NotEmpty(b1, b2)
+
+	a.NotError(srv.Transfer("1", "2"))
+	b2, err = srv.bucket(r2)
+	a.NotError(err).NotNil(b2)
+	a.Equal(b1, b2)
+}
