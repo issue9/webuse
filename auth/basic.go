a953e8adf1e5fdd280bc1b23df5584018dfacb55
caixw <xnotepad@gmail.com>
2018-12-28 01:09:55 +0800

    添加对错误的处理

--- auth/basic.go
+++ auth/basic.go
@@ -6,6 +6,7 @@ package auth
 
 import (
 	"encoding/base64"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -14,13 +15,14 @@ type basic struct {
 	next   http.Handler
 	secret string
 	realm  string
+	errlog *log.Logger
 
 	authorization string
 	authenticate  string
 }
 
 // NewBasic 新的 Basic 验证方式
-func NewBasic(next http.Handler, username, password, realm string, proxy bool) http.Handler {
+func NewBasic(next http.Handler, username, password, realm string, proxy bool, log *log.Logger) http.Handler {
 	authorization := "Authorization"
 	authenticate := "WWW-Authenticate"
 	if proxy {
@@ -32,6 +34,7 @@ func NewBasic(next http.Handler, username, password, realm string, proxy bool) h
 		next:   next,
 		secret: base64.StdEncoding.EncodeToString([]byte(username + ":" + password)),
 		realm:  `Basic realm="` + realm + `"`,
+		errlog: log,
 
 		authorization: authorization,
 		authenticate:  authenticate,
@@ -42,15 +45,18 @@ func (b *basic) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	auth := r.Header.Get(b.authorization)
 	index := strings.IndexByte(auth, ' ')
 
-	if index <= 0 || auth[:index] != "Basic" {
+	if index <= 0 || index >= len(auth) || auth[:index] != "Basic" {
 		b.unauthorization(w)
 		return
 	}
 
 	secret, err := base64.StdEncoding.DecodeString(auth[index+1:])
 	if err != nil {
-		// TODO
+		b.errlog.Println(err)
+		b.unauthorization(w)
+		return
 	}
+
 	if string(secret) != b.secret {
 		b.unauthorization(w)
 		return
