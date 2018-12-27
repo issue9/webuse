35fcd549a84933754cccfbbf7de98e969c4e4f77
caixw <xnotepad@gmail.com>
2018-12-28 01:01:46 +0800

    添加 proxy 功能

--- auth/basic.go
+++ auth/basic.go
@@ -14,19 +14,32 @@ type basic struct {
 	next   http.Handler
 	secret string
 	realm  string
+
+	authorization string
+	authenticate  string
 }
 
 // NewBasic 新的 Basic 验证方式
-func NewBasic(next http.Handler, username, password, realm string) http.Handler {
+func NewBasic(next http.Handler, username, password, realm string, proxy bool) http.Handler {
+	authorization := "Authorization"
+	authenticate := "WWW-Authenticate"
+	if proxy {
+		authorization = "Proxy-Authorization"
+		authenticate = "Proxy-Authenticate"
+	}
+
 	return &basic{
 		next:   next,
 		secret: base64.StdEncoding.EncodeToString([]byte(username + ":" + password)),
 		realm:  `Basic realm="` + realm + `"`,
+
+		authorization: authorization,
+		authenticate:  authenticate,
 	}
 }
 
 func (b *basic) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	auth := r.Header.Get("Authorization")
+	auth := r.Header.Get(b.authorization)
 	index := strings.IndexByte(auth, ' ')
 
 	if index <= 0 {
@@ -52,6 +65,6 @@ func (b *basic) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (b *basic) unauthorization(w http.ResponseWriter) {
-	w.Header().Set("WWW-Authenticate", b.realm)
+	w.Header().Set(b.authenticate, b.realm)
 	http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 }
