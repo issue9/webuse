67eec09d6f1150a0264194433ade99ad29301094
caixw <xnotepad@gmail.com>
2019-03-20 22:11:24 +0800

    改进文档

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/issue9/middleware
 
 require (
 	github.com/bradfitz/gomemcache v0.0.0-20180710155616-bc664df96737
-	github.com/issue9/assert v1.3.1
+	github.com/issue9/assert v1.3.2
 	github.com/issue9/rands v1.1.2
 	github.com/issue9/unique v1.1.3
 	github.com/issue9/utils v1.0.2
--- recovery/errorhandler/response.go
+++ recovery/errorhandler/response.go
@@ -18,8 +18,11 @@ func (r *response) WriteHeader(status int) {
 	r.ResponseWriter.WriteHeader(status)
 }
 
-// WriteHeader 如果不想让 400 以上的状态码被作特殊处理，
-// 可以调用此方法逃避。
+// WriteHeader 写入 HTTP 状态值
+//
+// 通过 errorhandler 的封装之后，默认会将大于等于 400
+// 状态值的响应重定向到指定的处理函数，如果不需要特殊处理，
+// 可以调用此函数，按照正常流程处理。
 func WriteHeader(w http.ResponseWriter, status int) {
 	if status < 400 {
 		w.WriteHeader(status)
